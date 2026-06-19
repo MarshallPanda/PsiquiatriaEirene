3c807302c20c9bc8947a3a7bd05590d875b45ac4
marshallpanda <jeimypando683@gmail.com>
2026-06-19 19:24:13 +0200

    ignore agregado

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp, getDoc, getDocs, doc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCTquDBYf4unSX1HFdlreyIr6_23zbbV1c",
-  authDomain: "eirele-psicologica.firebaseapp.com",
-  projectId: "eirele-psicologica",
-  storageBucket: "eirele-psicologica.firebasestorage.app",
-  messagingSenderId: "742536880237",
-  appId: "1:742536880237:web:7f064ffbfd2d784e6e5f64"
-};
-
+import { firebaseConfig } from './config.js';
+// Luego inicializas normal...
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
